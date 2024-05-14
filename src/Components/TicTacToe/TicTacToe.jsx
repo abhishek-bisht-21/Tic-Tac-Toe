de48ae2b7a8009fd6114bc4177c3b1d432feb814
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-14 22:55:19 +0530

    Added Rows for tictactoe game

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -4,15 +4,32 @@ import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png'
 
 const TicTacToe = () => {
-  return (
-   <div className='container'>
-	<h1 className='title'> Tic Tac Toe In <span>React</span></h1>
-	<div className='board'>
+	return (
+		<div className='container'>
+			<h1 className='title'> Tic Tac Toe In <span>React</span></h1>
+			<div className='board'>
+				<div className='row1'>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+				</div>
 
-	</div>
-	<button className='reset'>Reset</button>
-    </div>
-  )
+				<div className='row2'>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+				</div>
+
+
+				<div className='row3'>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+					<div className='boxes'></div>
+				</div>
+			</div>
+			<button className='reset'>Reset</button>
+		</div>
+	)
 }
 
 export default TicTacToe
