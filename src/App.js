5e6d94841df48dc6348b42eb77233fe76489a5e4
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-14 22:52:03 +0530

    Added Style to the Tic Tac toe game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import TicTacToe from './Components/TicTacToe/TicTacToe'; 
 import './App.css';
 
 function App() {
   return (
     <div>
+    <TicTacToe />
     </div>
   );
 }
