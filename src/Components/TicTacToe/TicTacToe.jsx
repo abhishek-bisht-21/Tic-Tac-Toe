86b51e9a42ca9dc769455eb08e0e8cfd9999ef0a
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-14 23:33:43 +0530

    Added the logic to Display Winner

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './TicTacToe.css'
 import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png'
@@ -11,6 +11,7 @@ const TicTacToe = () => {
 
 	let [count, setCount] = useState(0);
 	let [lock, setLock] = useState(false);
+	let titleRef  = useRef(null);
 
 
 	const toggle = (e, num) => {
@@ -33,44 +34,49 @@ const TicTacToe = () => {
 
 	const checkWin = () => {
 		if(data[0] === data[1] && data[1] === data[2] && data[2] !== ""){
-			won(data);
+			won(data[2]);
 		}else if(data[3] === data[4] && data[4] === data[5] && data[5] !== ""){
-			won(data);
+			won(data[5]);
 		}
 		else if(data[6] === data[7] && data[7] === data[8] && data[8] !== ""){
-			won(data);
+			won(data[8]);
 		}
 		else if(data[0] === data[3] && data[3] === data[6] && data[6] !== ""){
-			won(data);
+			won(data[6]);
 		}
 
 		else if(data[1] === data[4] && data[4] === data[7] && data[7] !== ""){
-			won(data);
+			won(data[7]);
 		}
 
 		else if(data[2] === data[5] && data[5] === data[8] && data[8] !== ""){
-			won(data);
+			won(data[8]);
 		}
 
 		else if(data[0] === data[4] && data[4] === data[8] && data[8] !== ""){
-			won(data);
+			won(data[8]);
 		}
 		else if(data[0] === data[1] && data[1] === data[2] && data[2] !== ""){
-			won(data);
+			won(data[2]);
 		}
 		else if(data[2] === data[4] && data[4] === data[6] && data[6] !== ""){
-			won(data);
+			won(data[6]);
 		}
 	}
 
 	const won = (winner) => {
 		setLock(true);
+		if (winner === "x") {
+			titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon}> Wins`;
+		}else{
+			titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon}> Wins`;
+		}
 	}
 
 
 	return (
 		<div className='container'>
-			<h1 className='title'> Tic Tac Toe In <span>React</span></h1>
+			<h1 className='title' ref={titleRef}> Tic Tac Toe In <span>React</span></h1>
 			<div className='board'>
 				<div className='row1'>
 					<div className='boxes' onClick={(e) => {toggle(e, 0)}}></div>
